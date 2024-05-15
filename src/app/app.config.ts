2c7ec5ecc4e2c8c240806a57abc2564b85a68050
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-05-15 17:58:13 +0200

    refacto: app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { WeatherService } from './services/weather.service';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), WeatherService],
+  providers: [provideRouter(routes), provideHttpClient()]
 };
